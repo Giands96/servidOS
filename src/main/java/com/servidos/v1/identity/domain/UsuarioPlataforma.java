adc7eb14951bb7a0d4b32d426718a54a6f9d0595
Axel <dsg4857@gmail.com>
2026-08-27 00:24:59 -0500

    add: Añadir Clase UsuarioPlataforma y sus campos

--- src/main/java/com/servidos/v1/identity/domain/UsuarioPlataforma.java
+++ src/main/java/com/servidos/v1/identity/domain/UsuarioPlataforma.java
@@ -1,4 +1,14 @@
 package com.servidos.v1.identity.domain;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class UsuarioPlataforma {
+    Long usuario_id;
+    Long rol_plataforma_id;
+    EstadoUsuario estado;
+    LocalDateTime created_at;
+    LocalDateTime updated_at;
 }
