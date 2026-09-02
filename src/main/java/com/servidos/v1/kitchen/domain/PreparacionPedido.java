9079eab70b3f884036fa1d551c4d4ac461b93b6f
Axel <dsg4857@gmail.com>
2026-09-01 23:28:21 -0500

    feat(kitchen): add PreparacionPedido projection

--- src/main/java/com/servidos/v1/kitchen/domain/PreparacionPedido.java
+++ src/main/java/com/servidos/v1/kitchen/domain/PreparacionPedido.java
@@ -1,18 +1,29 @@
 package com.servidos.v1.kitchen.domain;
 
+import com.servidos.v1.ordering.domain.DetallePedido;
 import com.servidos.v1.ordering.domain.EstadoPedido;
+import com.servidos.v1.ordering.domain.TipoPedido;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class PreparacionPedido {
     private Long pedido_id;
     private Long restaurante_id;
+    private Long mesa_id;
+    private TipoPedido tipoPedido;
     private EstadoPedido estado;
+    private String observacion;
+    private BigDecimal total;
     private LocalDateTime created_at;
+    private List<DetallePedido> items;
 }
