60a15ec84e4d899c3badbe62a7a83600586ab5ec
Axel <dsg4857@gmail.com>
2026-09-07 13:01:42 -0500

    feat(application): habilitar propiedades de configuración de autenticación en V1Application

--- src/main/java/com/servidos/v1/V1Application.java
+++ src/main/java/com/servidos/v1/V1Application.java
@@ -1,9 +1,12 @@
 package com.servidos.v1;
 
+import com.servidos.v1.identity.infrastructure.AuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AuthProperties.class)
 public class V1Application {
 
 	public static void main(String[] args) {
