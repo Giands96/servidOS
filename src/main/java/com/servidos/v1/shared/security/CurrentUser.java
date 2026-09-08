55d9c5935553abece9ccb68efa6d2bd9b4508748
Axel <dsg4857@gmail.com>
2026-09-08 18:14:25 -0500

    feat(auth): cambia tipo de datos de rol a String en manejo de usuario actual

--- src/main/java/com/servidos/v1/shared/security/CurrentUser.java
+++ src/main/java/com/servidos/v1/shared/security/CurrentUser.java
@@ -3,21 +3,21 @@ package com.servidos.v1.shared.security;
 
 public class CurrentUser {
     public static final ThreadLocal<Long> currentUser = new ThreadLocal<Long>();
-    public static final ThreadLocal<Long> currentRole = new ThreadLocal<Long>();
+    public static final ThreadLocal<String> currentRole = new ThreadLocal<String>();
 
     public static void setCurrentUser(Long userId) {
         currentUser.set(userId);
     }
 
-    public static void setRole(Long roleId) {
-        currentRole.set(roleId);
+    public static void setRole(String role) {
+        currentRole.set(role);
     }
 
     public static Long getCurrentUser() {
         return currentUser.get();
     }
 
-    public static Long getRole() {
+    public static String getRole() {
         return currentRole.get();
     }
 
