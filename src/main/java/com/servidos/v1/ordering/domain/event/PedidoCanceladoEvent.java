5b9c0fe34f9ee8280498162ef8d1108ce320beea
Axel <dsg4857@gmail.com>
2026-09-17 17:01:20 -0500

    feat(ordering): bloquea cancelar pedido pagado y publica evento (TDD 11/11)

--- src/main/java/com/servidos/v1/ordering/domain/event/PedidoCanceladoEvent.java
+++ src/main/java/com/servidos/v1/ordering/domain/event/PedidoCanceladoEvent.java
@@ -1,4 +1,11 @@
 package com.servidos.v1.ordering.domain.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class PedidoCanceladoEvent {
+    private final Long pedidoId;
+    private final Long restauranteId;
 }
